0164de9ec2c1e2acdc66956cba227a86a8d50a3f
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2017-11-14 23:52:32 -0200

    server

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
-//definition of the variables used
-const 
-    express = require('express'),
-    app = express(),
-    https = require('https'),
-    http = require('http'),
-    options = {},
-    serverPort = 4443,
-    server = https.createServer(options, app),
-    io = require('socket.io')(server);
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var open = require('open');
+var options = {
+  key: fs.readFileSync('./fake-keys/privatekey.pem'),
+  cert: fs.readFileSync('./fake-keys/certificate.pem')
+};
+var serverPort = (process.env.PORT  || 3000);
+var https = require('https');
+var http = require('http');
+var server;
+if (process.env.LOCAL) {
+  server = https.createServer(options, app);
+} else {
+  server = http.createServer(app);
+}
+var io = require('socket.io')(server);
 
+var roomList = {};
 
-//server listening on port 4443
-server.listen(serverPort, function(){
-    console.log('server up and running at %s port', serverPort);
+app.get('/', function(req, res){
+  console.log('get /');
+  res.sendFile(__dirname + '/index.html');
 });
-//open room listing
-let openRoomListing = {};
-
-
-// app.get('/', function(req, res){
-//   console.log('get /');
-//   res.sendFile(__dirname + '/index.html');
-// });
+server.listen(serverPort, function(){
+  console.log('server up and running at %s port', serverPort);
+  if (process.env.LOCAL) {
+    open('https://localhost:' + serverPort)
+  }
+});
 
 function socketIdsInRoom(name) {
-  var socketIds = io.nsps['/'].adapter.rooms[name];
-  if (socketIds) {
-    var collection = [];
-    for (var key in socketIds) {
-      collection.push(key);
-    }
-    return collection;
-  } else {
-    return [];
-  }
+  return roomList[name];
 }
 
-//client connects to the server
-io.on('connection', (socket) => {
+io.on('connection', function(socket){
   console.log('connection');
-  socket.on('disconnect', () => {
+  socket.on('disconnect', function(){
     console.log('disconnect');
     if (socket.room) {
       var room = socket.room;
@@ -48,19 +45,30 @@ io.on('connection', (socket) => {
     }
   });
 
-  socket.on('join', (name, callback) => {
+  socket.on('join', function(name, callback){
     console.log('join', name);
+    let room = roomList[name] || [];
+    room.push(socket.id);
+    roomList[name] = room;
     var socketIds = socketIdsInRoom(name);
-    if(callback === 'function')
-        callback(socketIds);
+    const obj = {
+      mySocketId: socket.id,
+      socketIds
+    }
+    callback(obj);
     socket.join(name);
     socket.room = name;
   });
 
 
-  socket.on('exchange', (data) => {
-    console.log('exchange', data);
+  socket.on('exchange', function(data){
     data.from = socket.id;
+    if(data.from === data.to) {
+      console.log('data.from === data.to');
+      return;
+    }
+    console.log('data.from', data.from);
+    console.log('data.to', data.to);
     var to = io.sockets.connected[data.to];
     to.emit('exchange', data);
   });
